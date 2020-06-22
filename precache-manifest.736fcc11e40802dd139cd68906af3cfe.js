3ee71b145677ac0188ff36ae3c33bd443e97bfe9
Muniyan <50628045+Muniyan@users.noreply.github.com>
2020-06-22 18:00:35 +0530

    Updates

---
+++ precache-manifest.736fcc11e40802dd139cd68906af3cfe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41af47baf8678eef97fcbb2724a87817",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b03029b302f0a294191c",
+    "url": "/portfolio/static/css/main.578fe9c1.chunk.css"
+  },
+  {
+    "revision": "5b8bfec6409f0afd592a",
+    "url": "/portfolio/static/js/2.06784ee8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.06784ee8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b03029b302f0a294191c",
+    "url": "/portfolio/static/js/main.2a167806.chunk.js"
+  },
+  {
+    "revision": "4bd91fadd2fd7eb0d6b8",
+    "url": "/portfolio/static/js/runtime-main.32e6bbb7.js"
+  },
+  {
+    "revision": "272113c6ea8035ada5864bb5a84a1786",
+    "url": "/portfolio/static/media/muniyan.272113c6.jpg"
+  },
+  {
+    "revision": "6ca318aeefa587877f1bcb7e854407c9",
+    "url": "/portfolio/static/media/muniyan.6ca318ae.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bec1ec2997ea5d19dadce94c2c68826b.js"
+  "/portfolio/precache-manifest.736fcc11e40802dd139cd68906af3cfe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
